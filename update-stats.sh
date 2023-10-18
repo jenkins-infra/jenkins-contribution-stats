344a5daac4d8d4c92e812d3c29de3a78fb47c31e
Jean-Marc Meessen <jean-marc@meessen-web.org>
2023-10-18 11:56:47 +0200

    update "update-stats" with the parameterized  data consolidation

--- update-stats.sh
+++ update-stats.sh
@@ -3,6 +3,7 @@
 set -e
 
 ./collect-missing-data.sh
-./consolidate-data.sh
+./consolidate-data.sh submissions
+./consolidate-data.sh comments
 ./submission-submitter-report.sh
 ./comment-commenter-report.sh
