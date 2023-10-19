0236e5b33a846bf57e4df7ea94679ad61f0ff13e
Jean-Marc Meessen <jean-marc@meessen-web.org>
2023-10-19 16:13:21 +0200

    Start implementing feature

--- update-stats.sh
+++ update-stats.sh
@@ -2,7 +2,15 @@
 
 set -e
 
-./collect-missing-data.sh
+# to process another org than Jenkins define the "target_org" variable
+# export target_org="toto"
+
+# pro tip: define it at console level and not in this file
+# export target_org="toto"
+
+./collect-missing-data.sh "$target_org"
+exit
+
 ./consolidate-data.sh submissions
 ./consolidate-data.sh comments
 ./submission-submitter-report.sh
