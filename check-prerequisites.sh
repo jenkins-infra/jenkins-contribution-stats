cd22985c0f1c9ba5192557b9c3995bfd6bf9df28
Jean-Marc Meessen <jean-marc@meessen-web.org>
2024-04-03 11:58:30 +0200

    Update minimal version for jenkins-top-submitters tool

--- check-prerequisites.sh
+++ check-prerequisites.sh
@@ -36,7 +36,7 @@ fi
 
 ## TODO: (code duplication) This could be refactored and moved to a bash sub routine
 # The target version of the jenkins-top-submitters tool that we want to have installed.
-top_target_version=1.2.7
+top_target_version=1.2.8
 
 # Fetch the currently installed version of the jenkins-top-submitters tool.
 # The 'awk' command is used to extract the version number from the output of the 'jenkins-top-submitters version' command.
