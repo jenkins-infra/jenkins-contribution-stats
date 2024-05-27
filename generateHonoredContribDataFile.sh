b46f6a2a86b2da8833f2f73335a310f779b90ce3
gounthar <gounthar@gmail.com>
2024-05-27 17:03:25 +0200

    fix(honor): Cleanup.

--- generateHonoredContribDataFile.sh
+++ generateHonoredContribDataFile.sh
@@ -64,8 +64,5 @@ fi
 # Print a message indicating that the honored contributor is being added.
 echo "Adding $honored_contributor as the honored contributor."
 
-#echo "GitHub ENV file is: $GITHUB_ENV"
-
 # Set the honored_contributor as an output variable using an environment file.
 echo "HONORED_CONTRIBUTOR=$honored_contributor" >> $GITHUB_ENV
-cat $GITHUB_ENV | cut -d'=' -f2
