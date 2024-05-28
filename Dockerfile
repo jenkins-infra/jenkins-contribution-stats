1e62667ee51494b045f8b33aab292fa0c82ddab4
Bruno Verachten <gounthar@gmail.com>
2024-05-28 10:12:13 +0200

    "Dockerisation" and contributor honor GitHub Action (#60)

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-# This Dockerfile is used to build an image for a Go application.
+# This Dockerfile is used as a minimum image for the Jenkins Top Submitters GitHub Action.
 
-# We start from the golang:1.22.1-bookworm base image.
+# We start from the golang:1.22.3-bookworm base image.
 FROM golang:1.22.3-bookworm
 
 # We declare an ARG for the GitHub token, which will be used for authentication.
@@ -8,9 +8,24 @@ ARG GITHUB_TOKEN
 # We set an environment variable with the GitHub token.
 ENV GITHUB_TOKEN=$GITHUB_TOKEN
 
+# We declare ARGs for the user ID and group ID.
+ARG USER_ID
+ARG GROUP_ID
+
 # We update the package lists for upgrades for packages that need upgrading, as well as new packages that have just come to the repositories.
-# We install datamash, git, and sudo packages.
-RUN apt update && apt install -y datamash git sudo
+# We install datamash, git, jq, sudo, and GitHub CLI.
+# We also check if wget is installed, if not, we install it.
+# We create a directory for the GitHub CLI keyring and download the keyring.
+# We add the GitHub CLI repository to the sources list.
+# We update the package lists again and install the GitHub CLI.
+RUN apt update && apt install -y datamash git jq sudo && \
+        (type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+        && sudo mkdir -p -m 755 /etc/apt/keyrings \
+        && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+        && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+        && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+        && sudo apt update \
+        && sudo apt install gh -y
 
 # We create a new user called linuxbrew with home directory and zsh as default shell.
 # We add the user to the sudo group.
@@ -55,3 +70,14 @@ RUN ln -s $(which date) /bin/gdate && \
     brew tap jmMeessen/tap && \
     brew install jenkins-stats && \
     brew install jenkins-top-submitters
+
+# Create a new user with the user ID and group ID.
+# If the group already exists, use the existing group.
+# If the user already exists, use the existing user.
+# Switch to the new user.
+# This is done to ensure that the Docker container runs with the same user and group IDs as the GitHub runner,
+# which helps to avoid permission issues when the Docker container writes to files that the GitHub runner needs to access.
+RUN if getent group $GROUP_ID ; then groupname=$(getent group $GROUP_ID | cut -d: -f1) ; else groupadd -g $GROUP_ID runner && groupname=runner ; fi && \
+    if getent passwd $USER_ID ; then username=$(getent passwd $USER_ID | cut -d: -f1) ; else useradd -l -u $USER_ID -g $groupname runner && username=runner ; fi && \
+    install -d -m 0755 -o $username -g $groupname /home/runner
+USER $username
--- generateHonoredContribDataFile.sh
+++ generateHonoredContribDataFile.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
-set -e
+# This script is used to honor a contributor based on their contributions in a specific month.
+# If no month is specified, it defaults to the previous month.
 
+set -e  # Exit immediately if a command exits with a non-zero status.
+
+# Check if the GitHub Personal Access token is defined.
+# If not, print an error message and exit the script.
 if [ -z ${GITHUB_TOKEN+x} ]; then echo "Error: the GitHub Personal Access token (\$GITHUB_TOKEN) is not defined. Aborting..."; exit; fi
 
-# check wether required tools are available
+# Check whether required tools are available.
+# This script is expected to be in the same directory as check-prerequisites.sh.
 ./check-prerequisites.sh
 
-
-# Make sure that we are using GNU Date on MacOS
+# Make sure that we are using GNU Date on MacOS.
+# If the script is running on MacOS, use gdate. Otherwise, use date.
 if [[ "$OSTYPE" == "darwin"* ]]; then
     gnu_date="gdate"
 else
@@ -17,9 +23,11 @@ fi
 
 month_to_process="$1"
 
-# Has month to process been specified? If not compute it. 
+# Has month to process been specified? If not compute it.
+# If the user did not provide a month to process, compute the previous month.
 if [ -z "$month_to_process" ];
 then
+    # Compute the year and month for the previous month.
     to_process_year=$(${gnu_date} -d "$(date +%Y-%m-1) -1 month" +%Y)
     to_process_month=$(${gnu_date} -d "$(date +%Y-%m-1) -1 month" +%m)
 
@@ -27,8 +35,44 @@ then
     echo "no specific target month supplied. Taking the previous month."
 fi
 
-# no specific parameter check: it is handled by the application
-echo "Picking the submitter to honor in ${month_to_process}"
-
-# perform the query and generates the data file data/honored_contributor.csv
+# Perform the query and generate the data file data/honored_contributor.csv.
+# The jenkins-stats command is expected to be in the PATH.
 jenkins-stats honor "$month_to_process" --data_dir=data/ -v
+
+# Add the current working directory to the list of directories that Git considers to be safe.
+# This is to prevent Git from refusing to work with files in the current directory.
+git config --global --add safe.directory "$PWD"
+
+# Add all changes to the staging area.
+# This prepares the changes for a commit.
+git add data/honored_contributor.csv
+
+# Read the third field (GitHub handle) from the honored_contributor.csv file
+# This is the GitHub handle of the honored contributor.
+github_handle=$(tail -n 1 data/honored_contributor.csv | cut -d',' -f3 | tr -d '[:space:]' | tr -d '"')
+
+# Read the fourth field (name) from the honored_contributor.csv file
+# This is the name of the honored contributor.
+honored_contributor=$(tail -n 1 data/honored_contributor.csv | cut -d',' -f4 | tr -d '[:space:]' | tr -d '"')
+
+# If the name is empty, use the GitHub handle
+# This is to ensure that the honored contributor is always identified.
+if [ -z "$honored_contributor" ]; then
+    honored_contributor=$github_handle
+fi
+
+# Print a message indicating that the honored contributor is being added.
+echo "Adding $honored_contributor as the honored contributor."
+
+# Check if the GITHUB_ENV variable is defined.
+# If not, create it using mktemp.
+if [ -z ${GITHUB_ENV+x} ]; then
+    GITHUB_ENV=$(mktemp)
+    echo -e "\e[36mGITHUB_ENV \e[33mis unset (I guess you're not in a GitHub action), so I'm creating a new temporary file \e[36m$GITHUB_ENV\e[33m so the script won't fail.\e[0m"
+    # Set a trap to delete the temporary file when the script exits.
+    trap "rm -f $GITHUB_ENV" EXIT
+else
+    echo "GITHUB_ENV is set to '$GITHUB_ENV'"
+fi
+# Set the honored_contributor as an output variable using an environment file.
+echo "HONORED_CONTRIBUTOR=$honored_contributor" >> $GITHUB_ENV
